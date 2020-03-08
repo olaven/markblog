6dd4ebe52f4d8d700104a076765b75529054303d
olaven <olav@sundfoer.com>
2020-03-08 13:27:57 +0100

    RSS data structure wiht channel (missing items++)

--- rss/rss.ts
+++ rss/rss.ts
@@ -1,3 +1,5 @@
+import { Tag } from "./serialize/serialize.ts"
+
 import { parse_xml } from "../deps.ts";
 import { read_file } from "../common.ts"
 /**
@@ -6,44 +8,62 @@ import { read_file } from "../common.ts"
  * the line
  */
 
-interface Attribute {
-    key: string, 
-    value: string, 
+
+//NOTE: exported for testing 
+export interface ChannelOptions {
+    title: string, 
+    link: string, 
+    description: string, 
+    language?: string, 
+    copyright?: string, 
+    managingEditor?: string, 
+    webmaster?: string, 
+    pubDate?: string, 
+    lastBuildDate?: string, 
+    category?: string, 
+    generator?: string, 
+    docs?: string, 
+    cloud?: string, 
+    ttl?: number, 
+    image?: string, 
+    rating?: string, 
+    textInput?: string, 
+    skipHours?: string, 
+    skipDays?: string,
 }
 
-export interface Tag {
-    name: string, 
-    children: Tag[] | string, 
-    attributes: Attribute[]
+//NOTE: exported for testing 
+export const get_channel = (options: ChannelOptions): Tag => {
+
+    //NOTE: options for a channel as specified as child tags/elements
+    const option_tags: Tag[] = Object.keys(options)
+        //@ts-ignore
+        .map(option => ({ name: option, children: options[option], attributes: []}))
+    //const items: Tag //TODO: items element
+
+    return {
+        name: "channel", 
+        children: [ ...option_tags/*, items*/ ], 
+        attributes: []
+    }
 }
 
-export const stringify_tag = (tag: Tag): string => {
-    
-    const content = (typeof(tag.children) === "string")? 
-        tag.children: 
-        tag.children.map(child => stringify_tag(child))
+//NOTE: exported for testing 
+export const get_rss = (options: ChannelOptions): Tag => {
 
-    const attributes = tag.attributes
-        .map(attribute => `${attribute.key}="${attribute.value}"`)
-        .join(" ");
-
-    //TODO: make more readable 
-    return `<${tag.name}${attributes.length > 0? ` ${attributes}`: ``}>${content}</${tag.name}>`
+    const channel = get_channel(options) 
+    return {
+        name: "rss", 
+        children: [ channel ], 
+        attributes: [
+            { key: "version", value: "2.0" }
+        ]
+    }
 }
 
-interface Channel extends Tag {
-    name: "channel", 
-    children: Tag[]
-}
-
-interface RSS extends Tag {
-    channel: Channel
-}
-
-const get_rss = () => {
-
-    
-}
+//TODO: include items in RSS data structure (right now, only channel is added (line 42))
+//TODO: write RSS to file 
+//TODO: combine serializing with get_rss in exposed API
 
 
 //const xml = await read_file("./feed.rss");
--- rss/rss_test.ts
+++ rss/rss_test.ts
@@ -1,85 +1,44 @@
-import { Tag, stringify_tag } from "./rss.ts"
+import { Tag } from "./serialize/serialize.ts"
+import { get_rss, get_channel, ChannelOptions } from "./rss.ts"
 import { assertEquals, assert } from "../deps.ts";
-const { test } = Deno 
+const { test } = Deno;
 
-test("can stringify tag", () => {
+const get_dummy_options = (
+    title = "blog title", 
+    link = "https://www.example.com", 
+    description = "Description of test blog") => ({
 
-    const tag: Tag = {
-        name: "my_tag_name", 
-        children: [
-            {
-                name: "sub_tag", 
-                children: "inner_content_of_tag", 
-                attributes: []
-            }
-        ],
-        attributes: []
-    }; 
+        title, link, description
+})
 
-    const actual = stringify_tag(tag);
-    const expected = `<my_tag_name><sub_tag>inner_content_of_tag</sub_tag></my_tag_name>`
+test("can get RSS", () => {
 
-    assertEquals(actual, expected)
+    const rss = get_rss(get_dummy_options())
+    assert(rss);
 });
 
-test("parsing includes attributes", () => {
+test("RSS has _one_ child", () => {
 
-    const tag: Tag = {
-        name: "tag", 
-        children: [], 
-        attributes: [
-            { key: "key1", value: "value1"},
-            { key: "key2", value: "value2"},
-        ]
-    }
-
-    const actual = stringify_tag(tag);
-    const expected = `<tag key1="value1" key2="value2"></tag>`;
-    assertEquals(actual, expected);
+    const rss = get_rss(get_dummy_options()); 
+    assertEquals(rss.children.length, 1); 
 });
 
-test("parsing includes attributes on children", () => {
+test("RSS's child is a 'channel'-tag", () => {
 
-    const tag: Tag = {
-        name: "parent", 
-        children: [
-            {
-                name: "child", 
-                children: [], 
-                attributes: [
-                    { key: "child_key", value: "child_value" }
-                ]
-            }
-        ], 
-        attributes: [
-            { key: "parent_key", value: "parent_value"}
-        ]
-    }
+    const channel = get_channel(get_dummy_options());
 
-    const actual = stringify_tag(tag);
-    const expected = `<parent parent_key="parent_value"><child child_key="child_value"></child></parent>`;
-    assertEquals(actual, expected);
+    assert(typeof(channel) !== "string"); 
+    assertEquals(channel.name, "channel");
 });
 
-test("parsing includes attributes on children _and_ string child", () => {
+test("Channel has specified option-elements", () => {
 
-    const tag: Tag = {
-        name: "parent", 
-        children: [
-            {
-                name: "child", 
-                children: "inner child value",
-                attributes: [
-                    { key: "child_key", value: "child_value" }
-                ]
-            }
-        ], 
-        attributes: [
-            { key: "parent_key", value: "parent_value"}
-        ]
-    }
+    const category = "Newspapers"
+    const options: ChannelOptions = {category, ...get_dummy_options()}
 
-    const actual = stringify_tag(tag);
-    const expected = `<parent parent_key="parent_value"><child child_key="child_value">inner child value</child></parent>`;
-    assertEquals(actual, expected);
-});
+    const channel = get_channel(options);
+    const tag = (channel.children as Tag[])
+        .find(tag => tag.name === "category" && tag.children === category && tag.attributes.length === 0);
+
+    assert(tag);
+})
