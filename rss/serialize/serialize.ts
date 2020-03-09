1bd8277b69cf47618136ba72561ff353491083bc
olaven <olav@sundfoer.com>
2020-03-09 14:32:55 +0100

    refactored serialize.ts

--- rss/serialize/serialize.ts
+++ rss/serialize/serialize.ts
@@ -9,19 +9,26 @@ export interface Tag {
     attributes: Attribute[]
 }
 
+const get_content = (tag: Tag) => {
+    
+    return (typeof(tag.children) === "string")? 
+        tag.children: 
+        tag.children.map(child => serialize(child))
+}
+
+const format_attributes = (tag: Tag) => tag.attributes
+    .map(attribute => `${attribute.key}="${attribute.value}"`)
+    .join(" ");
+
+
 /**
  * Serialze the tag-data structure 
  * to a string. 
  */
 export const serialize = (tag: Tag): string => {
     
-    const content = (typeof(tag.children) === "string")? 
-        tag.children: 
-        tag.children.map(child => serialize(child))
-
-    const attributes = tag.attributes
-        .map(attribute => `${attribute.key}="${attribute.value}"`)
-        .join(" ");
+    const content = get_content(tag);
+    const attributes = format_attributes(tag);
 
     //TODO: make more readable 
     return `<${tag.name}${attributes.length > 0? ` ${attributes}`: ``}>${content}</${tag.name}>`
