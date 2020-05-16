d71aed23f956a14957208a5085b15f8459a2f8ec
olaven <olav@sundfoer.com>
2020-05-16 11:23:45 +0200

    updated CI to 1.0

--- source/deps.ts
+++ source/deps.ts
@@ -16,4 +16,4 @@ export {
   assertThrows,
   assertThrowsAsync,
   assertStrContains
-} from "https://deno.land/std@v1.0.0-rc1/testing/asserts.ts";
+} from "https://deno.land/std/testing/asserts.ts";
